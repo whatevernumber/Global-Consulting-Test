f215e8b86b085431fe44891bcaaa90a3bb0538c1
whatevernumber <achford@mail.ru>
2024-05-08 13:01:42 +0300

    Statuses migration updated

--- database/migrations/2024_05_03_103051_create_statuses_table.php
+++ database/migrations/2024_05_03_103051_create_statuses_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->enum('value', ['available', 'unavailable']);
             $table->tinyText('name');
-            $table->timestamps();
         });
     }
 
